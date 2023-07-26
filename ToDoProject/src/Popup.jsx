1b8ecaa5714b7371f23e7238a7d55387bc2f6a8c
Slicepop <46600908+Slicepop@users.noreply.github.com>
2023-07-26 15:03:40 -0400

    changed task style with a tab style

--- ToDoProject/src/Popup.jsx
+++ ToDoProject/src/Popup.jsx
@@ -8,13 +8,13 @@ export default function Popup(){
     return (
         <div className='popup' id='popup'>
             <div className="window">
-                <img src= ".//xIcon.png" alt="close" className='closeIcon' onClick={close}/>
-                <div className="top">
+                <div className="banner">
                     <h1>Title</h1>
-                    <div className="topRight">
-                        <p>project</p>
-                        <p>date</p>
-                    </div>
+                    <img src= ".//xIcon.png" alt="close" className='closeIcon' onClick={close}/>
+                </div>
+                <div className="top">
+                    <p>project</p>
+                    <p>date</p>
                 </div>
                 <textarea readOnly className='window--desc'></textarea>
                 <p className='window--priority'>Priority: High</p>
